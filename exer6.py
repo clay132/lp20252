4f2b4eca50e1c7b6394bb73ee12d70420c63d840
clay132 <clay.pierre@icloud.com>
2025-08-18 01:13:12 +0000

    sucessor e antecessor de numeros inteiro

---
+++ exer6.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um número inteiro e imprima o seu
+#   antecessor e o seu sucessor.
+numero = int(input ("digite um numero inteiro:"))
+sucessor= numero + 1
+antecessor = numero - 1
+print ("sucessor:", sucessor,"antecessor:",antecessor)
