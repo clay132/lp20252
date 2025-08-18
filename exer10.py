0c524735f3d160d13e8154d648592e28db01ca2b
clay132 <clay.pierre@icloud.com>
2025-08-18 11:39:05 +0000

    a media aritmetica de float

---
+++ exer10.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia três números reais e calcule a
+#    média aritmética destes números. Ao final, o programa deve
+#    imprimir o resultado do cálculo.
+numero1= float (input ("digita o primeiro numero:"))
+numero2= float(input("digita o sengundo numero:"))
+numero3= float (input("digita o terceiro numero:"))
+soma= numero1+ numero2+numero3
+resultado = soma/3
+print ("a media aritmetica dos numeros:",numero1,numero2,numero3, "é:",resultado)
