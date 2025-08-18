3543e1f33ccb9043295151e98a9b53de7ec560f0
clay132 <clay.pierre@icloud.com>
2025-08-18 01:33:05 +0000

    armazenar informaçao de um clinte

---
+++ exer7.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia o nome o endereço e o telefone de
+#   um cliente e ao final, imprima esses dados.
+nome =(input("qual é o seu nome: "))
+ende= (input ("qual é o seu endereço: "))
+telef= (input ("digite o seu telefone: "))
+print ("o nome do cliente é:",nome,"o endereço do é:",ende,"telefone:",telef)
