bf78578abb5f1395b4f82658d570bdc617dfddad
clay132 <clay.pierre@icloud.com>
2025-08-18 00:31:00 +0000

    Faça um programa que leia dois números reais e os imprima.

---
+++ exer5.py
@@ -0,0 +1,4 @@
+#5. Faça um programa que leia dois números reais e os imprima.
+nur1 = float(input("escreva um numero reais:"))
+nur2 = float (input("escreva um numero reais :"))
+print("o primeiro numero reais é:",nur1,"o segundo numero é:", nur2)
