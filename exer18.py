baa0d562e5b5f651c8170ee4751261a90d559fde
clay132 <clay.pierre@icloud.com>
2025-08-18 12:57:16 +0000

    calculando distancia percorido e combustivo comsumido

---
+++ exer18.py
@@ -0,0 +1,18 @@
+#18. Faça um programa que calcule a quantidade de litros de combustível
+#    consumidos em uma viagem, sabendo-se que o carro tem autonomia de
+#    12 km por litro de combustível. O programa deverá ler o tempo
+#    decorrido na viagem e a velocidade média e aplicar as fórmulas:
+#    D = T x V       L = D / 12
+#    Em que:
+#    • D = Distância percorrida em horas
+#    • T = Tempo
+#    • V = Velocidade média
+#    • L = Litros de combustível consumidos
+#    Ao final, o programa deverá imprimir a distância percorrida e a
+#    quantidade de litros consumidos na viagem.
+t=int(input("qual  foi o tempo decorrido na viagem: "))
+v = int (input("qual é a velocidade media :"))
+D = (t*v)
+L= (D/12)
+print (D)
+print(L)
