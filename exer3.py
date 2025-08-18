25072690e1c5c47e8e05c93ea5e3209f4bf9080f
clay132 <clay.pierre@icloud.com>
2025-08-18 00:02:11 +0000

    média aritmética entre os números 5, 8, 12

---
+++ exer3.py
@@ -0,0 +1,7 @@
+#3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
+nume1 = 5
+nume2 = 8
+nume3= 12
+soma = nume2+nume3+nume1
+aritmetica = soma // 3
+print ("a media aritmetica dos numeros 5,8,12 é:",aritmetica )
