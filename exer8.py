c23deaada9a0e27dce51f7b207c3a73aafbf4005
clay132 <clay.pierre@icloud.com>
2025-08-18 11:22:41 +0000

    subitraçao entre dois numeros inteiros

---
+++ exer8.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia dois números inteiros e imprima a
+#   subtração deles.
+print("os numeros que vc escrevera sera subtraido entre eles ")
+nmr1= int(input ("escreva um numero inteiro:"))
+nmr2=int(input("escreva um numero inteiro:"))
+print(nmr1- nmr2)
